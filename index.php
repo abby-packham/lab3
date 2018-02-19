5b894476a149adb95fb88eacfd4ec1b6dc303128
Abby Packham <apackham@csumb.edu>
2018-02-19 17:36:30 +0000

    lab 3 working

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$deck = array();
+$deckValues = array();
+$deckSuits = array();
+$suitArray = array("Clubs", "Diamonds", "Hearts", "Spades");
+$cardTypes = array('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack','Queen','King');
+$playerNames = array("Abby", "David", "Sammy", "Mike");
+$players = array();
+
+
+?>
