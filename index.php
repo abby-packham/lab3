79d616d1ebc00edc9a58de99f61a54267b044f44
Abby Packham <apackham@csumb.edu>
2018-02-27 06:22:06 +0000

    all functions done

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ $cardTypes = array('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack','
 
 $playerNames = array("Abby", "David", "Sammy", "Mike");
 $players = array();
+$scores = array();
+$winner;
+$maxScore = 0;
+
 
 
 
 function shuffleDeck(){
+    
+    global $deck;
     //initialize deck
     for($i = 0; $i < 51; $i++){
         $deck[$i] = $i;
@@ -28,6 +34,8 @@ function shuffleDeck(){
 }
 //deal the player their hand
 function getHand(){
+    global $deck;
+    
     $currentHandValue = 0;
     $hand = array();
     while($currentHandValue <= 37){
@@ -39,21 +47,53 @@ function getHand(){
 }
 
 function drawCard(){
+    global $deck, $deckPointer;
     $returnValue = $deck[$deckPointer];
     $deckPointer++;
     return $returnValue;
 }
 
 function displayHand(){
+    global $players, $suitArray, $winner, $scores;
+    foreach($players as $key => $playerHand){
+        echo "$key: ";
+        
+        foreach($playerHand as $card){
+            $suit = $suitArray[$card/13];
+            $face = ($card % 13) + 1;
+            echo "<img src=\"./cards/$suit/$face.png\" >";
+        }
+        $player = $scores[$key];
+        echo "score: $player";
+        echo "<br/>";
+    }
     
+    echo "The winner is: $winner";
 }
 
 function findWinner() {
-    
+        global $players, $scores, $winner;
+    foreach($players as $key => $playerHand){
+        $playerScore = 0;
+        foreach($playerHand as $card){
+            $playerScore += ($card % 13) + 1;
+        }
+        $scores[$key] = $playerScore;
+        
+    }
+    foreach($scores as $key => $score){
+        if($score > $maxScore&& $score <= 42){
+            $maxScore = $score;
+            $winner = $key;
+         }
+    }
 }
 
 // entry point for the web page
 shuffleDeck();
+
+
+
 
 foreach($playerNames as &$player){
     $players[$player] = getHand();
@@ -66,6 +106,10 @@ foreach($playerNames as &$player){
         <title> Silver Jack </title>
     </head>
     <body>
-
+        <?php
+        findWinner();
+        displayHand();
+        ?>
+        
     </body>
 </html>
